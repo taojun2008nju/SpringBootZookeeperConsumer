3b2bba3c445264a99892a1918a3619a5ecfefae6
TaoJun0319 <taojun2008nju@163.com>
2020-11-28 17:19:57 +0800

    集成hystrix组件

--- src/main/java/com/test/ApiApplication.java
+++ src/main/java/com/test/ApiApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {FreeMarkerAutoConfiguration.class})
 @ComponentScan(basePackages = {"com.test"})
 @EnableDubbo
+@EnableHystrix
 public class ApiApplication extends SpringBootServletInitializer {
 
     @Override
--- src/main/java/com/test/CommonApiController.java
+++ src/main/java/com/test/CommonApiController.java
@@ -1,6 +1,8 @@
 package com.test;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.test.service.TestService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +18,25 @@ public class CommonApiController {
 
     @RequestMapping(value = "/testApi")
     @ResponseBody
+    @HystrixCommand(
+        fallbackMethod = "testApiFallback",
+        ignoreExceptions = {NullPointerException.class, ArithmeticException.class},
+        commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000" )
+        })
     public String testApi() throws Exception {
         log.info("Method:testApi");
-        testService.test("123");
-        return "Hello World";
+        String testResult = testService.test("123");
+        return testResult;
+    }
+
+    /**
+     * 降级fallback方法
+     * @return
+     * @throws Exception
+     */
+    public String testApiFallback(Throwable throwable) throws Exception {
+        log.error("Method:testApiFallback exception:", throwable);
+        return "testApiFail FailBack";
     }
 }
